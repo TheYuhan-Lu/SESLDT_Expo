550255cda8d40fd16cd0ffc37a7218ac220f800d
TheYuhan-Lu <theyuhanlu@gmail.com>
2024-03-01 22:21:55 -0800

    Added expo-camera dependency

---
+++ Screens/PatientScreen.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { Camera } from 'expo-camera';
+
+// For testing hardware set
