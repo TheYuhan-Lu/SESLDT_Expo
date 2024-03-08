7c9764913eb9b9c14b651bc48b68b9948b6d3f06
Yang Li <lee16613@126.com>
2024-03-08 12:57:00 -0800

    modified:   App.js 	new file:   GoogleService-Info.plist 	modified:   app.json 	new file:   google-services.json 	modified:   yarn.lock

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import AuthScreen from './Screens/AuthScreen';
 import PatientHomeScreen from './Screens/PatientHomeScreen';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CameraScreen from './Screens/CameraScreen';
 
-const Tab = createBottomTabNavigator();
+//const Tab = createBottomTabNavigator();
 
 export default function App() {
   console.log("App executed");
 
   return (
-    <CameraScreen />
+    <AuthScreen />
 
   );
 } 
